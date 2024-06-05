a0062839ab47f58383941079410797688041178c
vanshika704 <sharmavanshi704@gmail.com>
2024-06-05 17:37:28 +0530

    ..

---
+++ lib/Eating duo.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class EatingDuo extends StatefulWidget {
+  const EatingDuo({super.key});
+
+  @override
+  State<EatingDuo> createState() => _EatingDuoState();
+}
+
+class _EatingDuoState extends State<EatingDuo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Row(
+          children: [
+            Image.asset(
+              "assets/logo.png",
+              height: 60,
+              width: 60,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            const Text(
+              "Dine Partner...",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 248, 202, 116),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/page1.dart
+++ lib/page1.dart
@@ -1,3 +1,6 @@
+import 'package:feast__friends/Eating%20duo.dart';
+import 'package:feast__friends/Eatingquadrates.dart';
+import 'package:feast__friends/Eatingtrio.dart';
 import 'package:flutter/material.dart';
 
 class Page1 extends StatefulWidget {
@@ -8,6 +11,35 @@ class Page1 extends StatefulWidget {
 }
 
 class _Page1State extends State<Page1> {
+  final List<String> gridTexts = [
+    "Eating Pair",
+    "Dining Trio",
+    "Dining Quartet",
+    "Four Dining Companions",
+  ];
+
+  void _onGridItemTap(BuildContext context, int index) {
+    Widget page;
+    switch (index) {
+      case 0:
+        page = const EatingDuo();
+        break;
+      case 1:
+        page = const EatingTrio();
+        break;
+      case 2:
+        page = const EatingQuadrates();
+        break;
+
+      default:
+        return;
+    }
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => page),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +56,7 @@ class _Page1State extends State<Page1> {
               width: 20,
             ),
             const Text(
-              "Choose Way..",
+              "Dine Partners",
               style: TextStyle(
                   color: Color.fromARGB(255, 248, 202, 116),
                   fontWeight: FontWeight.bold,
@@ -33,23 +65,48 @@ class _Page1State extends State<Page1> {
           ],
         ),
       ),
-      body: Container(
-        color: Colors.black,
-        child: GridView.builder(
-          padding: const EdgeInsets.all(50.0),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 1,
-            mainAxisSpacing: 10.0,
-            childAspectRatio: 4 / 1,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/Untitled design (6).png",
+              fit: BoxFit.cover,
+            ),
           ),
-          itemCount: 6,
-          itemBuilder: (context, index) {
-            return Container(
-              color: const Color.fromARGB(255, 248, 202, 116),
-              margin: const EdgeInsets.symmetric(vertical: 3.0),
-            );
-          },
-        ),
+          Container(
+            color: Colors.black.withOpacity(0),
+            child: Center(
+              child: GridView.builder(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(50.0),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1,
+                  mainAxisSpacing: 10.0,
+                  childAspectRatio: 4 / 1,
+                ),
+                itemCount: gridTexts.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () => _onGridItemTap(context, index),
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: const Color.fromARGB(255, 248, 202, 116),
+                      margin: const EdgeInsets.symmetric(vertical: 3.0),
+                      child: Text(
+                        gridTexts[index],
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
