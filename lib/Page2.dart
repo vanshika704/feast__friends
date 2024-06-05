43c34dad990bcfb301d5ff3a969dd6d41df90530
vanshika704 <sharmavanshi704@gmail.com>
2024-06-05 19:23:21 +0530

    .

--- lib/Page2.dart
+++ lib/Page2.dart
@@ -1,13 +1,42 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  List<String> foodImages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFoodImages();
+  }
+
+  Future<void> fetchFoodImages() async {
+    final response = await http.get(Uri.parse(
+        'https://api.unsplash.com/photos/random?query=food&count=3&client_id=-SXu-p1sLlhOb9e6jqiKCfP46WmqjCl3DeGLt_L2-tw'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      final List<String> images = [];
+      for (final item in data) {
+        final imageUrl = item['urls']['regular'];
+        images.add(imageUrl);
+      }
+      setState(() {
+        foodImages = images;
+      });
+    } else {
+      throw Exception('Failed to load images');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +49,10 @@ class _MainPageState extends State<MainPage> {
               height: 60,
               width: 60,
             ),
-            const SizedBox(
+            SizedBox(
               width: 20,
             ),
-            const Text(
+            Text(
               "Welcome",
               style: TextStyle(
                 color: Color.fromARGB(255, 248, 202, 116),
@@ -34,54 +63,43 @@ class _MainPageState extends State<MainPage> {
           ],
         ),
       ),
-      backgroundColor: Colors.black, // Set background color of Scaffold
+      backgroundColor: Colors.black,
       body: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         padding: const EdgeInsets.all(16.0),
         child: Row(
           children: [
-            _buildCard('assets/logo.png', 'Profile 1'),
-            _buildCard('assets/logo.png', 'Profile 2'),
-            _buildCard('assets/logo.png', 'Profile 3'),
+            _buildCard(foodImages.isNotEmpty ? foodImages[0] : ''),
+            _buildCard(foodImages.isNotEmpty ? foodImages[1] : ''),
+            _buildCard(foodImages.isNotEmpty ? foodImages[2] : ''),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildCard(String imagePath, String text) {
+  Widget _buildCard(String imageUrl) {
     return Card(
-      color: Colors.grey[300],
+      color: const Color.fromARGB(110, 224, 224, 224),
       child: InkWell(
         onTap: () {
-          // Handle card tap
-          print('Card tapped: $text');
+          print('Card tapped: $imageUrl');
         },
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Row(
-            children: [
-              ClipOval(
-                child: Image.asset(
-                  imagePath,
-                  height: 50,
-                  width: 50,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Text(
-                text,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
+          child: imageUrl.isNotEmpty
+              ? Container(
+                  height: 200, // Adjust height as needed
+                  width: 200, // Adjust width as needed
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                )
+              : Container(), // Return an empty container if imageUrl is empty
         ),
       ),
     );
