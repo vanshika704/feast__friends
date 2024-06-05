856c7ee9be7b8b75facbb76f5af8050f8dcf72da
vanshika704 <sharmavanshi704@gmail.com>
2024-06-05 18:41:32 +0530

    ..

--- lib/Page2.dart
+++ lib/Page2.dart
@@ -24,13 +24,64 @@ class _MainPageState extends State<MainPage> {
               width: 20,
             ),
             const Text(
-              "Dine Partners",
+              "Welcome",
               style: TextStyle(
-                  color: Color.fromARGB(255, 248, 202, 116),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30),
-            )
+                color: Color.fromARGB(255, 248, 202, 116),
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
           ],
+        ),
+      ),
+      backgroundColor: Colors.black, // Set background color of Scaffold
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            _buildCard('assets/logo.png', 'Profile 1'),
+            _buildCard('assets/logo.png', 'Profile 2'),
+            _buildCard('assets/logo.png', 'Profile 3'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(String imagePath, String text) {
+    return Card(
+      color: Colors.grey[300],
+      child: InkWell(
+        onTap: () {
+          // Handle card tap
+          print('Card tapped: $text');
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  imagePath,
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
