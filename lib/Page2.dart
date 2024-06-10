eeded33ed75b83d789ecf2a5be3ff45930316af0
vanshika704 <sharmavanshi704@gmail.com>
2024-06-10 23:05:04 +0530

    ..

--- lib/Page2.dart
+++ lib/Page2.dart
@@ -1,6 +1,5 @@
-import 'dart:ui'; // Add this line
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -29,11 +28,17 @@ class _MainPageState extends State<MainPage> {
               const SizedBox(width: 3),
               SizedBox(
                 width: MediaQuery.of(context).size.width - 100,
-                child: const TabBar(
+                child: TabBar(
                   isScrollable: false,
                   indicatorSize: TabBarIndicatorSize.label,
-                  labelColor: Color.fromARGB(255, 252, 221, 85),
-                  unselectedLabelColor: Color.fromARGB(255, 253, 253, 253),
+                  labelStyle: GoogleFonts.dmSerifText(
+                      textStyle: TextStyle(
+                    color: Color.fromARGB(255, 252, 221, 85),
+                  )),
+                  unselectedLabelStyle: GoogleFonts.dmSerifText(
+                      textStyle: TextStyle(
+                    color: Color.fromARGB(255, 253, 253, 253),
+                  )),
                   labelPadding: EdgeInsets.symmetric(horizontal: 8.0),
                   indicator: BoxDecoration(),
                   tabs: [
@@ -53,28 +58,56 @@ class _MainPageState extends State<MainPage> {
             children: [
               Column(
                 children: [
-                  Image.asset(
-                    'assets/food1.jpg',
-                    fit: BoxFit.cover,
+                  Stack(
+                    children: [
+                      Image.asset(
+                        'assets/food1.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        top: 50,
+                        left: 50,
+                        child: Text(
+                          "Life is too Short for \n     Boring food",
+                          style: GoogleFonts.dmSerifText(
+                              textStyle: TextStyle(
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 247, 244, 244),
+                          )),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
               Column(
                 children: [
                   Text('Explore',
-                      style: TextStyle(fontSize: 50, color: Colors.white)),
+                      style: GoogleFonts.dmSerifText(
+                          textStyle: TextStyle(
+                        fontSize: 50,
+                        color: Colors.white,
+                      ))),
                 ],
               ),
               Column(
                 children: [
                   Text('Partner',
-                      style: TextStyle(fontSize: 50, color: Colors.white)),
+                      style: GoogleFonts.dmSerifText(
+                          textStyle: TextStyle(
+                        fontSize: 50,
+                        color: Colors.white,
+                      ))),
                 ],
               ),
               Column(
                 children: [
                   Text('About',
-                      style: TextStyle(fontSize: 50, color: Colors.white)),
+                      style: GoogleFonts.dmSerifText(
+                          textStyle: TextStyle(
+                        fontSize: 50,
+                        color: Colors.white,
+                      ))),
                 ],
               ),
             ],
