9b82f464a6b221df478359541f1be9b5a8ec7e0f
vanshika704 <sharmavanshi704@gmail.com>
2024-06-23 10:41:33 +0530

    changes

--- lib/Page2.dart
+++ lib/Page2.dart
@@ -19,9 +19,9 @@ class _MainPageState extends State<MainPage> {
     print('Find Partner tapped');
   }
 
-  void _onThirdContainerTap() {
-    // Perform action for the third container
-    print('Third container tapped');
+  void _onOrderTap() {
+    // Perform action for the order container
+    print('Order tapped');
   }
 
   @override
@@ -47,13 +47,15 @@ class _MainPageState extends State<MainPage> {
                   isScrollable: false,
                   indicatorSize: TabBarIndicatorSize.label,
                   labelStyle: GoogleFonts.dmSerifText(
-                      textStyle: const TextStyle(
-                    color: Color.fromARGB(255, 252, 221, 85),
-                  )),
+                    textStyle: const TextStyle(
+                      color: Color.fromARGB(255, 252, 221, 85),
+                    ),
+                  ),
                   unselectedLabelStyle: GoogleFonts.dmSerifText(
-                      textStyle: const TextStyle(
-                    color: Color.fromARGB(255, 253, 253, 253),
-                  )),
+                    textStyle: const TextStyle(
+                      color: Color.fromARGB(255, 253, 253, 253),
+                    ),
+                  ),
                   labelPadding: const EdgeInsets.symmetric(horizontal: 8.0),
                   indicator: const BoxDecoration(),
                   tabs: const [
@@ -71,166 +73,220 @@ class _MainPageState extends State<MainPage> {
           color: Colors.black,
           child: TabBarView(
             children: [
-              Column(
-                children: [
-                  Stack(
-                    children: [
-                      Image.asset(
-                        'assets/food7.jpg',
-                        fit: BoxFit.cover,
-                        height: 300,
-                        width: 500,
-                      ),
-                      Positioned(
-                        top: 80,
-                        left: 50,
-                        child: Text(
-                          "Life is too Short for \n     Boring food",
-                          style: GoogleFonts.dmSerifText(
-                              textStyle: const TextStyle(
-                            fontSize: 30,
-                            color: Color.fromARGB(255, 247, 244, 244),
-                          )),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: 400,
-                    height: 100,
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(207, 83, 83, 83),
-                    ),
-                    child: Row(
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Stack(
                       children: [
-                        Expanded(
-                          child: InkWell(
-                            onTap: _onExploreTap,
-                            child: Container(
-                              decoration: const BoxDecoration(
-                                border: Border(
-                                  top: BorderSide(
-                                    color: Colors.white,
-                                    width: 3.0,
-                                  ),
-                                  right: BorderSide(
-                                    color: Colors.white,
-                                    width: 1.0,
-                                  ),
-                                ),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.location_on,
-                                    color: Colors.white,
-                                  ),
-                                  SizedBox(height: 8),
-                                  Text(
-                                    'Explore',
-                                    style: GoogleFonts.dmSerifText(
-                                      textStyle: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+                        Image.asset(
+                          'assets/food7.jpg',
+                          fit: BoxFit.cover,
+                          height: 300,
+                          width: 500,
                         ),
-                        Expanded(
-                          child: InkWell(
-                            onTap: _onFindPartnerTap,
-                            child: Container(
-                              decoration: const BoxDecoration(
-                                border: Border(
-                                  top: BorderSide(
-                                    color: Colors.white,
-                                    width: 3.0,
-                                  ),
-                                  right: BorderSide(
-                                    color: Colors.white,
-                                    width: 1.0,
-                                  ),
-                                ),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.people,
-                                    color: Colors.white,
-                                  ),
-                                  SizedBox(height: 8),
-                                  Text(
-                                    'Find Partner',
-                                    style: GoogleFonts.dmSerifText(
-                                      textStyle: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: InkWell(
-                            onTap: _onThirdContainerTap,
-                            child: Container(
-                              decoration: const BoxDecoration(
-                                border: Border(
-                                  top: BorderSide(
-                                    color: Colors.white,
-                                    width: 3.0,
-                                  ),
-                                ),
+                        Positioned(
+                          top: 80,
+                          left: 50,
+                          child: Text(
+                            "Life is too Short for \n     Boring food",
+                            style: GoogleFonts.dmSerifText(
+                              textStyle: const TextStyle(
+                                fontSize: 30,
+                                color: Color.fromARGB(255, 247, 244, 244),
                               ),
                             ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    Container(
+                      width: 400,
+                      height: 100,
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(207, 83, 83, 83),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: InkWell(
+                              onTap: _onExploreTap, // Function reference
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                    top: BorderSide(
+                                      color: Colors.white,
+                                      width: 3.0,
+                                    ),
+                                    right: BorderSide(
+                                      color: Colors.white,
+                                      width: 1.0,
+                                    ),
+                                  ),
+                                ),
+                                child: const Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.location_on,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      'Explore',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: InkWell(
+                              onTap: _onFindPartnerTap, // Function reference
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                    top: BorderSide(
+                                      color: Colors.white,
+                                      width: 3.0,
+                                    ),
+                                    right: BorderSide(
+                                      color: Colors.white,
+                                      width: 1.0,
+                                    ),
+                                  ),
+                                ),
+                                child: const Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.people,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      'Find Partner',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: InkWell(
+                              onTap: _onOrderTap, // Function reference
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                    top: BorderSide(
+                                      color: Colors.white,
+                                      width: 3.0,
+                                    ),
+                                  ),
+                                ),
+                                child: const Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.fastfood,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      'Order',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Color.fromARGB(255, 94, 93, 93),
+                              width: 2, 
+                            ),
+                          ),
+                          child: Image.asset(
+                            "assets/pic2.png",
+                            height: 200,
+                            width: 180,
+                          ),
+                        ),
+                        SizedBox(
+                            width:
+                                20), // Adjust the width as needed for spacing
+                        Text(
+                          "A Good Meal\n  is All You\n      Need..",
+                          style: GoogleFonts.dmSerifText(
+                            textStyle: const TextStyle(
+                              fontSize: 25,
+                              color: Color.fromARGB(255, 247, 244, 244),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-              Column(
-                // explore page
-                children: [
-                  Text('Explore',
-                      style: GoogleFonts.dmSerifText(
-                          textStyle: const TextStyle(
+              const SingleChildScrollView(
+                child: Column(
+                  // explore page
+                  children: [
+                    Text(
+                      'Explore',
+                      style: TextStyle(
                         fontSize: 50,
                         color: Colors.white,
-                      ))),
-                ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              Column(
-                // partner page
-                children: [
-                  Text('Partner',
-                      style: GoogleFonts.dmSerifText(
-                          textStyle: const TextStyle(
+              const SingleChildScrollView(
+                child: Column(
+                  // partner page
+                  children: [
+                    Text(
+                      'Partner',
+                      style: TextStyle(
                         fontSize: 50,
                         color: Colors.white,
-                      ))),
-                ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              Column(
-                // about page
-                children: [
-                  Text('About',
-                      style: GoogleFonts.dmSerifText(
-                          textStyle: const TextStyle(
+              const SingleChildScrollView(
+                child: Column(
+                  // about page
+                  children: [
+                    Text(
+                      'About',
+                      style: TextStyle(
                         fontSize: 50,
                         color: Colors.white,
-                      ))),
-                ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
